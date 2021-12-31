c03bbd47b5c4fe15c24beb4aa539df85ccc6fa5b
양태웅 <didxodnd0422@gmail.com>
2021-12-31 14:51:09 +0900

    Apply DataBinding in MainActivity

--- app/src/main/java/com/yang/a09p9/MainActivity.kt
+++ app/src/main/java/com/yang/a09p9/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.yang.a09p9
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.yang.a09p9.base.BaseActivity
+import com.yang.a09p9.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+@AndroidEntryPoint
+class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun init() {
+        
     }
+
 }
