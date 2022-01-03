54552858d9204a91c7df33ab6ec60183efa5cc01
양태웅 <didxodnd0422@gmail.com>
2022-01-03 22:51:47 +0900

    Implement RegistrationFragment design

--- app/src/main/java/com/yang/a09p9/presentation/user/registration/RegistrationFragment.kt
+++ app/src/main/java/com/yang/a09p9/presentation/user/registration/RegistrationFragment.kt
@@ -1,13 +1,23 @@
-package com.yang.a09p9
+package com.yang.a09p9.presentation.user.registration
 
+import android.content.Intent
+import androidx.navigation.fragment.findNavController
+import com.yang.a09p9.R
 import com.yang.a09p9.base.BaseFragment
 import com.yang.a09p9.databinding.FragmentRegistrationBinding
+import com.yang.a09p9.presentation.MainActivity
 
 
 class RegistrationFragment  : BaseFragment<FragmentRegistrationBinding>(R.layout.fragment_registration) {
 
 
     override fun init() {
-
+        binding.registrationFragment = this
     }
+
+    fun toLogin() = findNavController().navigate(R.id.action_registrationFragment_to_loginFragment)
+
+//    fun toTerms() = findNavController().navigate(R.id.action_)
+    
+    fun onRegister() { startActivity(Intent(requireContext(), MainActivity::class.java)) }
 }
