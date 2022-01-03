c0c9533611378efa54af0c0de7167580af2d0f1f
양태웅 <didxodnd0422@gmail.com>
2022-01-03 16:24:19 +0900

    Add Login/Registration Fragment

---
+++ app/src/main/java/com/yang/a09p9/LoginFragment.kt
@@ -0,0 +1,12 @@
+package com.yang.a09p9
+
+import com.yang.a09p9.base.BaseFragment
+import com.yang.a09p9.databinding.FragmentLoginBinding
+
+class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
+
+
+    override fun init() {
+
+    }
+}
---
+++ app/src/main/java/com/yang/a09p9/RegistrationFragment.kt
@@ -0,0 +1,13 @@
+package com.yang.a09p9
+
+import com.yang.a09p9.base.BaseFragment
+import com.yang.a09p9.databinding.FragmentRegistrationBinding
+
+
+class RegistrationFragment  : BaseFragment<FragmentRegistrationBinding>(R.layout.fragment_registration) {
+
+
+    override fun init() {
+
+    }
+}
