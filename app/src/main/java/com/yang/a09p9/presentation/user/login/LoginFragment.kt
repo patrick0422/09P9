36df15a394810e2bcc69f708d9ec535901c97382
양태웅 <didxodnd0422@gmail.com>
2022-01-03 22:51:24 +0900

    Implement LoginFragment design

--- app/src/main/java/com/yang/a09p9/presentation/user/login/LoginFragment.kt
+++ app/src/main/java/com/yang/a09p9/presentation/user/login/LoginFragment.kt
@@ -1,12 +1,20 @@
-package com.yang.a09p9
+package com.yang.a09p9.presentation.user.login
 
+import android.content.Intent
+import androidx.navigation.fragment.findNavController
+import com.yang.a09p9.R
 import com.yang.a09p9.base.BaseFragment
 import com.yang.a09p9.databinding.FragmentLoginBinding
+import com.yang.a09p9.presentation.MainActivity
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(R.layout.fragment_login) {
 
 
     override fun init() {
-
+        binding.loginFragment = this
     }
+
+    fun toRegister() = findNavController().navigate(R.id.action_loginFragment_to_registrationFragment)
+
+    fun onLogin() { startActivity(Intent(requireContext(), MainActivity::class.java)) }
 }
