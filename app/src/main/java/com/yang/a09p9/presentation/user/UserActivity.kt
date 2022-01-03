67c5e7b450082981b7384f826aa9aa65eb630def
양태웅 <didxodnd0422@gmail.com>
2022-01-03 16:13:13 +0900

    Create UserActivity

---
+++ app/src/main/java/com/yang/a09p9/presentation/user/UserActivity.kt
@@ -0,0 +1,13 @@
+package com.yang.a09p9.presentation.user
+
+import com.yang.a09p9.R
+import com.yang.a09p9.base.BaseActivity
+import com.yang.a09p9.databinding.ActivityUserBinding
+
+class UserActivity : BaseActivity<ActivityUserBinding>(R.layout.activity_user) {
+
+    override fun init() {
+
+    }
+
+}
