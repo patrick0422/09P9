9e7f8c2209efbf0523d9ac68a4caf3b939478e81
양태웅 <didxodnd0422@gmail.com>
2022-01-04 17:38:06 +0900

    Add MainFragment

---
+++ app/src/main/java/com/yang/a09p9/MainFragment.kt
@@ -0,0 +1,10 @@
+package com.yang.a09p9
+
+import com.yang.a09p9.base.BaseFragment
+import com.yang.a09p9.databinding.FragmentMainBinding
+
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+    override fun init() {
+
+    }
+}
