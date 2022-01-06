70e0df430afb3c0ccdf6fa0962e29aabb4099439
양태웅 <didxodnd0422@gmail.com>
2022-01-06 10:05:25 +0900

    Rename Classes
    용어 통일성을 위해 변경
    login → sign in, logout → sign out
    registration → sign up

--- app/src/main/java/com/yang/a09p9/presentation/user/registration/RegistrationViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.yang.a09p9.presentation.user.registration
-
-import androidx.lifecycle.ViewModel
-
-
-class RegistrationViewModel: ViewModel() {
-
-}
---
+++ app/src/main/java/com/yang/a09p9/presentation/user/sign_up/SignUpViewModel.kt
@@ -0,0 +1,8 @@
+package com.yang.a09p9.presentation.user.sign_up
+
+import androidx.lifecycle.ViewModel
+
+
+class SignUpViewModel: ViewModel() {
+
+}
